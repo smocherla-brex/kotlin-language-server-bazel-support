8075235f929e8d2e82409cde22d34e21554158d5
Sridhar Mocherla <smocherla@brex.com>
2025-04-10 12:32:00 -0400

    Disable test in CI that requires indexing

--- server/src/test/kotlin/org/javacs/kt/bazel/QuickFixTest.kt
+++ server/src/test/kotlin/org/javacs/kt/bazel/QuickFixTest.kt
@@ -4,11 +4,13 @@ import org.eclipse.lsp4j.CodeActionKind
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.hasSize
 import org.javacs.kt.BazelLanguageServerTextFixture
+import org.junit.Ignore
 import org.junit.Test
 
 class QuickFixTest: BazelLanguageServerTextFixture("src/QuickFix.kt", indexingEnabled = true) {
 
     @Test
+    @Ignore("Disabled because github actions doesn't have DB setup yet")
     fun TestMissingImportsFix() {
         val only = listOf(CodeActionKind.QuickFix)
         val params = codeActionParams("src/QuickFix.kt", 3, 9, 3, 10, diagnostics,only)
